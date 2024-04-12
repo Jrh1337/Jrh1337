229d84bd0f7166045158690da43815703767efcb
Jrh1337 <166570231+Jrh1337@users.noreply.github.com>
2024-04-12 14:26:48 -0400

    Update Activity2.cpp

--- CMPSC121/Activity2.cpp
+++ CMPSC121/Activity2.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
 
+{
+	int num1, num2, num3, sum, quot;
+	cout << "Enter 3 integers" << endl;
+	cin >> num1 >> num2 >> num3;
+	if (num3 != 0)
+	{
+		sum = num1 + num2;
+		quot = sum / num3;
+		cout << num1 << "+" << num2 << "/" << num3 << "==" << quot << endl;
+
+	}
+	else
+	{
+
+		cout << "Unable to divide by zero" << endl;
+
+	}
+
+	return 0;
+}
