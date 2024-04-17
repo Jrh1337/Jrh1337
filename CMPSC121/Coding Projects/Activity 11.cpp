afa9fe8b8b9d4e824f2e204a30b375040188cc16
Jrh1337 <166570231+Jrh1337@users.noreply.github.com>
2024-04-17 13:01:28 -0400

    Update Activity 11.cpp

--- CMPSC121/Coding Projects/Activity 11.cpp
+++ CMPSC121/Coding Projects/Activity 11.cpp
@@ -1,107 +1,74 @@
-//Activity 16 
-//Purpose: This program asks for user withdrawals and deposits and keeps record of the total balance.  Program doesn't allow user to overdraft there account.
+//Activity 11 
+//Purpose: This opens a text file and calculates number of parts by type and value of each inventory
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <iomanip>
 using namespace std;
 
-double accountBalance(double); //holds a static value for the total balance from multiple user inputs
-
 int main()
 {
-	double transaction = 0, balance = 0;
-	char reply;
+	ifstream inputFile; //reads the input of a file 
+	string partno;
+	double Avalue = 0, Bvalue = 0, Cvalue = 0, Dvalue = 0, unknownValue = 0, cost; 
+	int quantity, Aparts = 0, Bparts = 0, Cparts = 0, Dparts = 0, unknownParts = 0;
+	char ch;
 
-	cout << setprecision(2) << fixed;
-	
-	do //Loop gathers first transaction and exits if user replies with anything other than y or Y
+	inputFile.open("C:\\Users\\John\\Desktop\\Hayes Documentation\\Classes\\College\\CMPSC 121\\cmpsc121\\homework\\Activity 11\\Activity11\\parts.txt"); //opens file path, change file path destination as necessary
+	if (inputFile.fail()) //output if file fails to open
 	{
-		cout << "Enter amount of transaction, positive for deposit, negative for withdrawal\n";
-		cin >> transaction;
+		cout << "Unable to Open File!";
+	}
+	else 
+	{
+		cout << setprecision(2) << fixed; 
+		while (inputFile >> partno >> ch >> quantity >> cost) //reads each file line and assignes values to variables seperated by spaces until no lines remain  
+		{
+			if (ch == 'A') //adds quantity of parts and total inventory values by parts based on what the character variable is assigned
+			{
+				Aparts++;
+				Avalue += quantity * cost;
+			}
+			else if (ch == 'B')
+			{
+				Bparts++;
+				Bvalue += quantity * cost;
+			}
+			else if (ch == 'C')
+			{
+				Cparts++;
+				Cvalue += quantity * cost;
+			}
+			else if (ch == 'D')
+			{
+				Dparts++;
+				Dvalue += quantity * cost;
+			}
+			else //For any parts besides A, B, C or D
+			{
+				unknownParts++;
+				unknownValue += quantity * cost;
+			}
 
-		balance = accountBalance(transaction); //Function passes each transaction as an argument and returns the total balance after each iteration.
+		}
+	}
+	cout << ".................INVENTORY REPORT..................\n"; //Final output for inventory report
+	cout << "A parts " << "Count: " << Aparts << " Value of inventory: " << Avalue << endl;
+	cout << "B parts " << "Count: " << Bparts << " Value of inventory: " << Bvalue << endl;
+	cout << "C parts " << "Count: " << Cparts << " Value of inventory: " << Cvalue << endl;
+	cout << "D parts " << "Count: " << Dparts << " Value of inventory: " << Dvalue << endl;
+	cout << "Unknown " << "Count: " << unknownParts << " Value of inventory: " << unknownValue << endl;
 
-		cout << "Current balance = " << balance << endl;
-		cout << "Do you have more transactions? Y or N?\n";
-		cin >> reply;
-
-	} while (toupper(reply) == 'Y');
-
-	cout << "Final balance = " << balance;
+	inputFile.close(); //closes the file
 
 	return 0;
-}
 
-double accountBalance(double transaction)
-{
-	static double balance;
-
-	if (transaction <= 0) //In the case of a withdrawal
-	{
-		balance += transaction; 
-		if (balance < 0) //If user attempts to overdraft from there account this if statement will execute if balance goes below zero.
-		{
-			balance -= transaction; //This statement returns the balance to the value it was before being overdrafted.
-			cout << "Impermissible withdrawal, insufficient funds!\n";
-			return balance;
-		}
-		else //Withdrawal that doesn't result in an overdraft
-		{
-			return balance;
-		}
-
-	}
-	else if (transaction > 0) //Executes for a deposit
-	{
-		balance += transaction;	
-		return balance;
-	}
-}
-
-/*
-Enter amount of transaction, positive for deposit, negative for withdrawal
-90
-Current balance = 90.00
-Do you have more transactions? Y or N?
-y
-Enter amount of transaction, positive for deposit, negative for withdrawal
--90
-Current balance = 0.00
-Do you have more transactions? Y or N?
-y
-Enter amount of transaction, positive for deposit, negative for withdrawal
-45
-Current balance = 45.00
-Do you have more transactions? Y or N?
-y
-Enter amount of transaction, positive for deposit, negative for withdrawal
-60
-Current balance = 105.00
-Do you have more transactions? Y or N?
-y
-Enter amount of transaction, positive for deposit, negative for withdrawal
--120
-Impermissible withdrawal, insufficient funds!
-Current balance = 105.00
-Do you have more transactions? Y or N?
-y
-Enter amount of transaction, positive for deposit, negative for withdrawal
-300
-Current balance = 405.00
-Do you have more transactions? Y or N?
-y
-Enter amount of transaction, positive for deposit, negative for withdrawal
--150
-Current balance = 255.00
-Do you have more transactions? Y or N?
-n
-Final balance = 255.00
+	/*
+	.................INVENTORY REPORT..................
+A parts Count: 85 Value of inventory: 191180.07
+B parts Count: 69 Value of inventory: 74764.16
+C parts Count: 77 Value of inventory: 50322.30
+D parts Count: 60 Value of inventory: 22416.49
+Unknown Count: 13 Value of inventory: 4282.68
 */
-
-
-/* Works if first input is overdraft 
-Enter amount of transaction, positive for deposit, negative for withdrawal
--30
-Impermissible withdrawal, insufficient funds!
-Current balance = 0.00
-Do you have more transactions? Y or N?
-*/
+}
