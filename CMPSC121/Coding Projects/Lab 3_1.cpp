e984a6d24bbd28e2baf4cf832ec102935e56a564
Jrh1337 <166570231+Jrh1337@users.noreply.github.com>
2024-04-19 12:05:17 -0400

    Create Lab 3_1.cpp

---
+++ CMPSC121/Coding Projects/Lab 3_1.cpp
@@ -0,0 +1,44 @@
+//Lab 03-1  
+//Purpose: This program takes the radius of the circle formed by a ring and the cross radius of the ring to calculate volume and surface area.
+//None
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+const double PI = 3.14159;
+
+int main()
+{
+	double crossR, ringR, volume, surfaceArea;
+
+	//Gather inputs set all values to third decimel place
+	cout << setprecision(3) << fixed;
+	cout << "Enter radius of ring and radius of cross section" << endl;
+	cin >> ringR >> crossR;
+
+	//Calculations of inputs
+	volume = 2 * pow(PI, 2.0) * ringR * pow(crossR, 2.0);
+	surfaceArea = 4 * pow(PI, 2.0) * crossR * ringR;
+
+	//Display information
+	cout << "For a ring with a radius of " << ringR << " and a cross section radius of " << endl;
+	cout << crossR << endl;
+
+	cout << setw(20) << left << "The volume is:" << setw(15) << right << volume << endl;
+	cout << setw(20) << left << "The surface area is:" << setw(15) << right << surfaceArea << endl;
+
+	return 0;
+
+	/*Enter radius of ring and radius of cross section
+25.75 3
+For a ring with a radius of 25.750 and a cross section radius of
+3.000
+The volume is:             4574.554
+The surface area is:       3049.703
+
+C:\Users\John\Desktop\Hayes Documentation\Classes\College\CMPSC 121\cmpsc121\Labs\Lab03\Lab03-1\Debug\Lab03-1.exe (process 23188) exited with code 0.
+To automatically close the console when debugging stops, enable Tools->Options->Debugging->Automatically close the console when debugging stops.
+Press any key to close this window . . .*/
+
+}
